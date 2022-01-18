3661f4894c628794dbb358e090587b18bc720443
gilbutitbook <gilbutitbook@gmail.com>
2022-01-18 13:31:29 +0900

    initial release

---
+++ README.md
@@ -0,0 +1,36 @@
+## <딥러닝 파이토치 교과서>(길벗, 2022) 도서의 예제 소스 파일입니다.
+
+∙Jupyter Notebook과 Colab용 파일을 제공합니다. </br>
+
+∙실습에 필요한 데이터셋 대부분은 챕터별 data 폴더에 들어 있습니다.</br>
+
+### =========================================
+
+### ∙아래 설명을 꼭 읽어주세요!!!
+
+∙10장 wiki.ko.vec만 별도로 내려받습니다.(약 2GB))</br>
+https://fasttext.cc/docs/en/pretrained-vectors.html 에서 Korea의 txt 파일 </br>
+
+∙내려받은 wiki.ko.vec 파일은 10장의 data 폴더 > wiki.co 파일에 넣어서 사용합니다.
+
+∙대용량 데이터셋은 깃허브에 올라가지 않습니다. 아래 링크에서 내려받아 각 챕터의 data 폴더에 넣어 실습하세요. </br>
+
+>∙ 10장 [glove.6B.100d.txt_내려받기](https://github.com/gilbutITbook/080263/releases/download/0.2/glove.6B.100d.txt) </br>
+   사이트 URL: https://nlp.stanford.edu/projects/glove/ </br>
+
+∙데이터셋은 python과 colab 모두 동일하게 사용하며, 일부 압축 파일은 colab 실습용입니다.</br>
+
+∙8장부터는 colab에서 '런타임 유형'을 'GPU'로 설정하고 실습하는 것이 좋습니다. (실행 시간 단축)</br>
+
+### =========================================
+∙colab에 필요한 데이터는 파일을 업로드해서 사용해야 합니다. 두 가지 방식이 있는데, 책에서는 대부분 PC에서 파일을 업로드하는 방식으로 되어 있습니다. </br>
+구글 드라이브를 사용 중이라면 드라이브에 마운트해 사용하는 것이 좀 더 편리합니다. (부록을 참고하세요)</br>
+
+#PC에서 파일 업로드 </br>
+from google.colab import files </br>
+file_uploaded=files.upload() </br>
+
+#구글 드라이브 마운트 </br>
+from google.colab import drive </br>
+drive.mount('/content/drive/') </br>
+
---
+++ chap08/es-python_08.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 8.3.3 조기종료
+
+
+# In[2]:
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+from torchvision import transforms, datasets
+
+import matplotlib
+import matplotlib.pyplot as plt
+import time
+import argparse
+from tqdm import tqdm
+matplotlib.style.use('ggplot')
+
+
+# In[3]:
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# In[4]:
+
+
+train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+val_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+
+# In[5]:
+
+
+train_dataset = datasets.ImageFolder(
+    root=r'../chap08/data/archive/train',
+    transform=train_transform
+)
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=32, shuffle=True,
+)
+val_dataset = datasets.ImageFolder(
+    root=r'../chap08/data/archive/test',
+    transform=val_transform
+)
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=32, shuffle=False,
+)
+
+
+# In[6]:
+
+
+def resnet50(pretrained=True):
+    model = models.resnet50(progress=True, pretrained=pretrained)
+    if requires_grad == False:
+        for param in model.parameters():
+            param.requires_grad = False
+    elif requires_grad == True:
+        for param in model.parameters():
+            param.requires_grad = True
+    model.fc = nn.Linear(2048, 2)
+    return model
+
+
+# In[7]:
+
+
+class LRScheduler():
+    def __init__(
+        self, optimizer, patience=5, min_lr=1e-6, factor=0.5
+    ):
+        self.optimizer = optimizer
+        self.patience = patience
+        self.min_lr = min_lr
+        self.factor = factor
+        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau( 
+                self.optimizer,
+                mode='min',
+                patience=self.patience,
+                factor=self.factor,
+                min_lr=self.min_lr,
+                verbose=True
+            )
+    def __call__(self, val_loss):
+        self.lr_scheduler.step(val_loss)
+
+
+# In[8]:
+
+
+class EarlyStopping():
+    def __init__(self, patience=5, verbose=False, delta=0, path='../chap08/data/checkpoint.pt'):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+        self.path = path
+        
+    def __call__(self, val_loss, model):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+
+# In[9]:
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr-scheduler', dest='lr_scheduler', action='store_true')
+parser.add_argument('--early-stopping', dest='early_stopping', action='store_true')
+parser.add_argument("-f", "--fff", help="a dummy argument to fool ipython", default="1") #주피터 노트북에서 실행할때 필요합니다
+args = vars(parser.parse_args())
+
+
+# In[10]:
+
+
+print(f"Computation device: {device}\n")
+model = models.resnet50(pretrained=True).to(device)
+total_params = sum(p.numel() for p in model.parameters())
+print(f"{total_params:,} total parameters.")
+total_trainable_params = sum(
+    p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{total_trainable_params:,} training parameters.")
+
+
+# In[11]:
+
+
+lr = 0.001
+epochs = 100
+optimizer = optim.Adam(model.parameters(), lr=lr)
+criterion = nn.CrossEntropyLoss()
+
+
+# In[12]:
+
+
+loss_plot_name = 'loss'
+acc_plot_name = 'accuracy'
+model_name = 'model'
+
+
+# In[13]:
+
+
+if args['lr_scheduler']:
+    print('INFO: Initializing learning rate scheduler')
+    lr_scheduler = LRScheduler(optimizer)
+    loss_plot_name = 'lrs_loss'
+    acc_plot_name = 'lrs_accuracy'
+    model_name = 'lrs_model'
+if args['early_stopping']:
+    print('INFO: Initializing early stopping')
+    early_stopping = EarlyStopping()
+    loss_plot_name = 'es_loss'
+    acc_plot_name = 'es_accuracy'
+    model_name = 'es_model'
+
+
+# In[14]:
+
+
+def training(model, train_dataloader, train_dataset, optimizer, criterion):
+    print('Training')
+    model.train()
+    train_running_loss = 0.0
+    train_running_correct = 0
+    counter = 0
+    total = 0
+    prog_bar = tqdm(enumerate(train_dataloader), total=int(len(train_dataset)/train_dataloader.batch_size))
+    for i, data in prog_bar:
+        counter += 1
+        data, target = data[0].to(device), data[1].to(device)
+        total += target.size(0)
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, target)
+        train_running_loss += loss.item()
+        _, preds = torch.max(outputs.data, 1)
+        train_running_correct += (preds == target).sum().item()
+        loss.backward()
+        optimizer.step()
+        
+    train_loss = train_running_loss / counter
+    train_accuracy = 100. * train_running_correct / total
+    return train_loss, train_accuracy
+
+
+# In[15]:
+
+
+def validate(model, test_dataloader, val_dataset, criterion):
+    print('Validating')
+    model.eval()
+    val_running_loss = 0.0
+    val_running_correct = 0
+    counter = 0
+    total = 0
+    prog_bar = tqdm(enumerate(test_dataloader), total=int(len(val_dataset)/test_dataloader.batch_size))
+    with torch.no_grad():
+        for i, data in prog_bar:
+            counter += 1
+            data, target = data[0].to(device), data[1].to(device)
+            total += target.size(0)
+            outputs = model(data)
+            loss = criterion(outputs, target)
+            
+            val_running_loss += loss.item()
+            _, preds = torch.max(outputs.data, 1)
+            val_running_correct += (preds == target).sum().item()
+        
+        val_loss = val_running_loss / counter
+        val_accuracy = 100. * val_running_correct / total
+        return val_loss, val_accuracy
+
+
+# In[16]:
+
+
+train_loss, train_accuracy = [], []
+val_loss, val_accuracy = [], []
+start = time.time()
+for epoch in range(epochs):
+    print(f"Epoch {epoch+1} of {epochs}")
+    train_epoch_loss, train_epoch_accuracy = training(
+        model, train_dataloader, train_dataset, optimizer, criterion
+    )
+    val_epoch_loss, val_epoch_accuracy = validate(
+        model, val_dataloader, val_dataset, criterion
+    )
+    train_loss.append(train_epoch_loss)
+    train_accuracy.append(train_epoch_accuracy)
+    val_loss.append(val_epoch_loss)
+    val_accuracy.append(val_epoch_accuracy)
+    if args['lr_scheduler']:
+        lr_scheduler(val_epoch_loss)
+    if args['early_stopping']:
+        early_stopping(val_epoch_loss, model)
+        if early_stopping.early_stop:
+            break
+    print(f"Train Loss: {train_epoch_loss:.4f}, Train Acc: {train_epoch_accuracy:.2f}")
+    print(f'Val Loss: {val_epoch_loss:.4f}, Val Acc: {val_epoch_accuracy:.2f}')
+end = time.time()
+print(f"Training time: {(end-start)/60:.3f} minutes")
+
+
+# In[17]:
+
+
+plt.figure(figsize=(10, 7))
+plt.plot(train_accuracy, color='green', label='train accuracy')
+plt.plot(val_accuracy, color='blue', label='validataion accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig(f"../chap08/img/{acc_plot_name}.png")
+plt.show()
+
+plt.figure(figsize=(10, 7))
+plt.plot(train_loss, color='orange', label='train loss')
+plt.plot(val_loss, color='red', label='validataion loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig(f"../chap08/img/{loss_plot_name}.png")
+plt.show()
+    
+print('Saving model...')
+torch.save(model.state_dict(), f"../chap08/img/{model_name}.pth")
+print('TRAINING COMPLETE')
+
+
+# In[ ]:
+
+
+
+
